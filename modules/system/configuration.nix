d1f43e142d7c68b9b4c0d58c1308c868cd16d022
Mads Bruun Jensen <madsj812@gmail.com>
2023-07-21 18:03:04 +0200

    some error trying to compile

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -5,17 +5,12 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   # Bootloader.
   boot.loader.grub.enable = true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
+  # networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
