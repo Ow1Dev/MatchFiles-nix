3a5856db53848d35746602664281ba321e71bf02
Mads Bruun Jensen <madsj812@gmail.com>
2023-07-23 13:24:54 +0200

    got it to work

--- modules/system/configuration.nix
+++ modules/system/configuration.nix
@@ -5,17 +5,26 @@
 { config, pkgs, ... }:
 
 {
+  services.xserver.desktopManager.xterm.enable = false;
+
+  environment.systemPackages = with pkgs; [
+     vim 
+     git
+     vscode    
+  ];
+
+  fonts.fonts = with pkgs; [
+    nerdfonts
+  ];
+
   # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
-  # networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  boot = {
+    loader = {
+	grub.enable = true;
+	grub.device = "/dev/sda";
+	grub.useOSProber = true;
+    };
+  };
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -25,7 +34,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_DK.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "da_DK.UTF-8";
     LC_IDENTIFICATION = "da_DK.UTF-8";
@@ -66,19 +74,8 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-  fonts.fonts = with pkgs; [
-    nerdfonts
-  ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ow1 = {
@@ -95,37 +92,41 @@
     ];
   };
 
+  # Security 
+  security = {
+    sudo.enable = false;
+    doas = {
+      enable = true;
+      extraRules = [{
+        users = [ "ow1" ];
+        keepEnv = true;
+        persist = true;
+      }];
+    };
+
+    # Extra security
+    protectKernelImage = true;
+  };
+
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     git
-     vscode    
-     # wget
-  ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # Set environment variables
+    environment.variables = {
+        NIXOS_CONFIG = "$HOME/.config/nixos/configuration.nix";
+        NIXOS_CONFIG_DIR = "$HOME/.config/nixos/";
+        XDG_DATA_HOME = "$HOME/.local/share";
+        PASSWORD_STORE_DIR = "$HOME/.local/share/password-store";
+        GTK_RC_FILES = "$HOME/.local/share/gtk-1.0/gtkrc";
+        GTK2_RC_FILES = "$HOME/.local/share/gtk-2.0/gtkrc";
+        MOZ_ENABLE_WAYLAND = "1";
+        EDITOR = "vim";
+        DIRENV_LOG_FORMAT = "";
+        ANKI_WAYLAND = "1";
+        DISABLE_QT5_COMPAT = "0";
+    };
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
