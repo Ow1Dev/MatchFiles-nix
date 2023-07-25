731ca878ed62810017464e40cbd98741c47071e3
Mads Bruun Jensen <madsj812@gmail.com>
2023-07-25 20:06:31 +0200

    nvim

---
+++ users/ow1/apps/nvim.nix
@@ -0,0 +1,23 @@
+{ pkgs
+, config
+, ...
+}: {
+  xdg.configFile."nvim".source = ../confs/nvim;
+
+  home.packages = with pkgs; [
+     nodejs
+     gcc
+  ];
+
+  programs.neovim = {
+    enable = true;
+    package = pkgs.neovim-unwrapped;
+    vimAlias = false;
+    viAlias = false;
+    vimdiffAlias = false;
+    withRuby = false;
+    withNodeJs = false;
+    withPython3 = false;
+  };
+}
+
