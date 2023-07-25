b1197ddccabbeed28aa1b216e9beee1d6bbdb1fd
Mads Bruun Jensen <madsj812@gmail.com>
2023-07-25 19:31:03 +0200

    waybar stuff

---
+++ users/ow1/apps/waybar.nix
@@ -0,0 +1,83 @@
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}: {
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        modules-left = ["hyprland/workspaces"];
+        modules-center = [];
+        modules-right = ["pulseaudio" "network" "battery" "clock" "tray"];
+
+        "hyprland/workspaces" = {
+          disable-scroll = true;
+          sort-by-name = true;
+          format = "{icon}";
+          format-icons = {default = "";};
+        };
+
+        pulseaudio = {
+          format = " {icon} ";
+          format-muted = "󰝟";
+          format-icons = ["" "󰕾" ""];
+          tooltip = true;
+          tooltip-format = "{volume}%";
+	  on-click = "amixer set Master toggle";
+        };
+
+        network = {
+          format-wifi = " ";
+          format-disconnected = "睊";
+          format-ethernet = " ";
+          tooltip = true;
+          tooltip-format = "{signalStrength}%";
+        };
+
+        backlight = {
+          device = "intel_backlight";
+          format = "{icon}";
+          format-icons = ["" "" "" "" "" "" "" "" ""];
+          tooltip = true;
+          tooltip-format = "{percent}%";
+        };
+
+        battery = {
+          states = {
+            warning = 30;
+            critical = 15;
+          };
+          format = "{icon}";
+          format-charging = "󰂄";
+          format-plugged = "󰂄";
+          format-icons = ["󰂎" "󰁺" "󰁻" "󰁼" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
+          tooltip = true;
+          tooltip-format = "{capacity}%";
+        };
+
+        clock = {
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+          format-alt = '' {:%d %m %Y}'';
+          format = '' {:%H %M}'';
+        };
+
+        tray = {
+          icon-size = 21;
+          spacing = 10;
+        };
+      };
+    };
+
+    style = builtins.readFile ../confs/waybar/style.css;
+  };
+
+  xdg.configFile."waybar/frappe.css".text =
+    builtins.readFile ../confs/waybar/frappe.css;
+}
