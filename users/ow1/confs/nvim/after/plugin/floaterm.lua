2cf6d503a9a6429f8fa99bfa5f514b5a69528b2f
Mads Bruun Jensen <madsj812@gmail.com>
2023-08-04 23:15:04 +0200

    feat(nvim): replace fugitive with lazygit

---
+++ users/ow1/confs/nvim/after/plugin/floaterm.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', '<leader>lg', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 lazygit<CR>', options)
+vim.keymap.set('n', '<leader>tt', '<CMD>FloatermNew --autoclose=2 --height=0.9 --width=0.9 zsh<CR>', options)
--- users/ow1/confs/nvim/lua/ow1/packer.lua
+++ users/ow1/confs/nvim/lua/ow1/packer.lua
@@ -17,11 +17,11 @@ require('packer').startup(function(use)
     }
 
     use { "catppuccin/nvim", as = "catppuccin" }
+    use 'voldikss/vim-floaterm'
 
     use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
-    use('tpope/vim-fugitive')
 
     use {
         'VonHeikemen/lsp-zero.nvim',
