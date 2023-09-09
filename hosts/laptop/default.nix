2d626fb4b926aa76a3a8fc4141cf6faa42f1c8e4
Mads Bruun Jensen <madsj812@gmail.com>
2023-09-09 23:01:20 +0200

    fix(gtk): add xdp portal with gtk

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -56,6 +56,11 @@
   programs.dconf.enable = true;
   programs.zsh.enable = true;
 
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  };
+
   # Configure keymap in X11
   services.xserver = {
     layout = "dk";
--- users/ow1/packages/default.nix
+++ users/ow1/packages/default.nix
@@ -7,6 +7,5 @@
     neovim-unwrapped
     zsh
     starship
-    dconf
   ];
 }
