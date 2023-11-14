d2868c522ae03d6fa779ceee4e3cca476564574d
Mads Bruun Jensen <madsj812@gmail.com>
2023-11-14 16:18:12 +0100

    fix(hypr): add screenshot util

--- users/ow1/apps/hyprland.nix
+++ users/ow1/apps/hyprland.nix
@@ -1,10 +1,26 @@
 { pkgs
+, lib
 , ...
-}: {
+}:
+let
+  _ = lib.getExe;
+  screenshotScript =
+    let
+      inherit (pkgs) wayshot slurp;
+    in
+    pkgs.writeShellScriptBin "wayshot-ss" ''
+      WORKSPACES="$(hyprctl monitors -j | jq -r 'map(.activeWorkspace.id)')"
+      WINDOWS="$(hyprctl clients -j | jq -r --argjson workspaces "$WORKSPACES" 'map(select([.workspace.id] | inside($workspaces)))' )"
+      GEOM=$(echo "$WINDOWS" | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"' | ${_ slurp} -f '%x %y %w %h')
+      ${_ wayshot} -s "$GEOM" --stdout ''${#:+"$@"}
+    '';
+in
+{
   home.packages = with pkgs; [
     hyprpaper
     brightnessctl
     wl-clipboard
+    screenshotScript
   ];
 
 
