1f0a82169f4d3829b7def48776c1bd81d176b94b
Mads Bruun Jensen <madsj812@gmail.com>
2024-01-18 20:59:51 +0100

    feat(waybar): move to a new style of bar

--- users/ow1/apps/waybar.nix
+++ users/ow1/apps/waybar.nix
@@ -1,8 +1,4 @@
-{ config
-, pkgs
-, lib
-, inputs
-, ...
+{ ...
 }: {
   programs.waybar = {
     enable = true;
@@ -10,40 +6,54 @@
       mainBar = {
         layer = "top";
         position = "top";
-        modules-left = [ "clock" ];
-        modules-center = [ "hyprland/workspaces" ];
-        modules-right = [ "pulseaudio" "network" "battery" "tray" ];
+        margin = "0 0 0 0";
+        modules-left = [ "hyprland/workspaces" "tray" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "backlight" "pulseaudio" "battery" "network" ];
 
         "hyprland/workspaces" = {
           disable-scroll = true;
-          sort-by-name = true;
+          all-outputs = true;
           format = "{icon}";
           format-icons = { default = ""; };
         };
 
-        pulseaudio = {
-          format = " {icon} ";
-          format-muted = "󰝟";
-          format-icons = [ "" "󰕾" "" ];
-          tooltip = true;
-          tooltip-format = "{volume}%";
-          on-click = "amixer set Master toggle";
+        tray = {
+          icon-size = 14;
+          spacing = 5;
         };
 
-        network = {
-          format-wifi = " ";
-          format-disconnected = "睊";
-          format-ethernet = " ";
-          tooltip = true;
-          tooltip-format = "{signalStrength}%";
+        clock = {
+          tooltip-format = "<tt><small>{calendar}</small></tt>";
+          calendar = {
+            mode = "month";
+            mode-mon-col = 3;
+            weeks-pos = "right";
+            on-scroll = 1;
+            on-click-right = "mode";
+            format = {
+              months = "<span color='#ffead3'><b>{}</b></span>";
+              days = "<span color='#ecc6d9'><b>{}</b></span>";
+              weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+              weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+              today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+            };
+            actions = {
+              on-click-right = "mode";
+              on-click-forward = "tz_up";
+              on-click-backward = "tz_down";
+              on-scroll-up = "shift_up";
+              on-scroll-down = "shift_down";
+            };
+          };
+          format = "  {:%a. %d %b.   %H:%M}";
+          format-alt = "  {:%d %m %Y   %H:%M}";
+          interval = 60;
         };
 
-        backlight = {
-          device = "intel_backlight";
-          format = "{icon}";
-          format-icons = [ "" "" "" "" "" "" "" "" "" ];
-          tooltip = true;
-          tooltip-format = "{percent}%";
+        memory = {
+          format = " {: >3}%";
+          on-click = "kitty -e htop";
         };
 
         battery = {
@@ -51,25 +61,42 @@
             warning = 30;
             critical = 15;
           };
-          format = "{icon}";
-          format-charging = "󰂄";
-          format-plugged = "󰂄";
-          format-icons = [ "󰂎" "󰁺" "󰁻" "󰁼" "󰁼" "󰁾" "󰁿" "󰂀" "󰂁" "󰂂" "󰁹" ];
-          tooltip = true;
-          tooltip-format = "{capacity}%";
+          format = "{icon}  {capacity: >3}%";
+          format-icons = [ "" "" "" "" "" ];
         };
 
-        clock = {
-          tooltip-format = ''
-            <big>{:%Y %B}</big>
-            <tt><small>{calendar}</small></tt>'';
-          format-alt = '' {:%d %m %Y}'';
-          format = '' {:%H %M}'';
+        network = {
+          format = "⚠  Disabled";
+          format-wifi = "  {essid}";
+          format-ethernet = "  Wired";
+          format-disconnected = "⚠  Disconnected";
+          on-click = "nm-connection-editor";
         };
 
-        tray = {
-          icon-size = 21;
-          spacing = 10;
+        pulseaudio = {
+          scroll-step = 1;
+          format = "{icon} {volume: >3}%";
+          format-bluetooth = "{icon} {volume: >3}%";
+          format-muted = " muted";
+          format-icons = {
+            headphones = "";
+            handsfree = "";
+            headset = "";
+            phone = "";
+            portable = "";
+            car = "";
+            default = [ "" "" ];
+          };
+          on-click = "pavucontrol";
+          on-scroll-up = "amixer set Master 5%+";
+          on-scroll-down = "amixer set Master 5%-";
+        };
+
+        backlight = {
+          format = "{icon} {percent: >3}%";
+          format-icons = ["" ""];
+          on-scroll-down = "brightnessctl s -5%";
+          on-scroll-up = "brightnessctl s +5%";
         };
       };
     };
